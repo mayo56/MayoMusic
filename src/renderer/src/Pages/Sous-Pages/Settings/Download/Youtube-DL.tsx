73e7545ffed6d062477d837dabcb328a4466e433
mayo <maxinou68.lr@gmail.com>
2024-11-05 16:55:22 +0100

    Update 0.3-alpha.3 Amélioration visuel de settings > download > yt-dlp

--- src/renderer/src/Pages/Sous-Pages/Settings/Download/Youtube-DL.tsx
+++ src/renderer/src/Pages/Sous-Pages/Settings/Download/Youtube-DL.tsx
@@ -2,12 +2,16 @@
 import React from 'react'
 
 // CSS
-import '@renderer/assets/CSS/Settings/Download/yt-dlp.css' // Icon
+import '@renderer/assets/CSS/Settings/Download/yt-dlp.css'
+
+// Icon
 import install_icon from '@renderer/assets/Images/hardware-chip-svgrepo-com.svg'
 import version_icon from '@renderer/assets/Images/git-compare-svgrepo-com.svg'
 import valid_icon from '@renderer/assets/Images/checkmark-circle-svgrepo-com.svg'
 import invalid_icon from '@renderer/assets/Images/close-circle-svgrepo-com.svg'
 import extract_icon from '@renderer/assets/Images/cloud-download-svgrepo-com.svg'
+import right_arrow_icon from '@renderer/assets/Images/chevron-forward-svgrepo-com.svg'
+import bottom_arrow_icon from '@renderer/assets/Images/chevron-down-svgrepo-com.svg'
 
 function YoutubeDLPage(): React.JSX.Element {
   const [isInstalled, setIsInstalled] = React.useState<boolean>(false)
@@ -24,20 +28,38 @@ function YoutubeDLPage(): React.JSX.Element {
   }, [])
 
   const [url, setURL] = React.useState<string>('')
-  const [audioQuality, setAudioQuality] = React.useState<string>('')
-  const [playlist, setPlaylist] = React.useState<boolean>(true)
+  const [audioQuality, setAudioQuality] = React.useState<string>('best')
+  const [playlist, setPlaylist] = React.useState<boolean>(false)
+  const [folderName, setFolderName] = React.useState<string>('')
 
+  // mise à jour auto bouton download
+  const [disabled, setDisabled] = React.useState<boolean>(true)
+  React.useEffect(() => {
+    if (url.trim() == '') {
+      setDisabled(true)
+    } else if (folderName.trim() == '') {
+      setDisabled(true)
+    } else {
+      setDisabled(false)
+    }
+  }, [url, folderName])
+
+  // Envoie des données de téléchargement
   const downloadRequest = (): void => {
+    if (disabled) return
     window.api.download.yt_dlp_download_req({
       url,
       quality: audioQuality,
       playlist,
-      folderName: 'Spice and Wolf: MERCHANT MEETS THE WISE WOLF (Original Soundtrack) (Volume 2)'
+      folderName
     })
   }
 
   // Data
   const [audioEXT] = React.useState<string[]>(['mp3', 'ogg', 'flac', 'm4a', 'aac'])
+
+  // Afficher les options avancées
+  const [showAdvanced, setShowAdvanced] = React.useState<boolean>(false)
   return (
     <div className={'yt-dlp-container'}>
       <div className={'yt-dlp-title'}>
@@ -66,63 +88,102 @@ function YoutubeDLPage(): React.JSX.Element {
       {/*
       Input argument de téléchargement
       */}
-      <div>
-        <h2>Informations principales</h2>
-        <span>URL</span>
-        <input
-          disabled={isInstalled === null ? true : !isInstalled}
-          type={'url'}
-          value={url}
-          onChange={(e) => setURL(e.target.value)}
-        />
+      <div className={'yt-dlp-important-input'}>
+        <div className={'yt-dlp-input-module'}>
+          <span className={'label'}>URL</span>
+          <input
+            disabled={isInstalled === null ? true : !isInstalled}
+            type={'url'}
+            value={url}
+            onChange={(e) => setURL(e.target.value)}
+            className={'texte-input'}
+            placeholder={'e.g. https://youtube.com/...'}
+          />
+        </div>
+
         {/*
-        Quality
+        Nom de dossier
         */}
-        <span>Audio Quality</span>
-        <select
-          value={audioQuality}
-          onChange={(e) => setAudioQuality(e.target.value)}
-          disabled={isInstalled === null ? true : !isInstalled}
-        >
-          <option value={''}>-- Selection qualité audio --</option>
-          <option value={'low'}>low</option>
-          <option value={'medium'}>medium</option>
-          <option value={'best'}>best</option>
-        </select>
+        <div className={'yt-dlp-input-module'}>
+          <span className={'label'}>Nom du dossier</span>
+          <input
+            disabled={isInstalled === null ? true : !isInstalled}
+            type={'text'}
+            value={folderName}
+            onChange={(e) => setFolderName(e.target.value)}
+            className={'texte-input'}
+            placeholder={'e.g. Pokemon'}
+          />
+        </div>
+
+        {/*
+        Audio Quality
+        */}
+        <div className={'yt-dlp-input-module'}>
+          <span className={'label'}>Audio Quality</span>
+          <select
+            value={audioQuality}
+            onChange={(e) => setAudioQuality(e.target.value)}
+            disabled={isInstalled === null ? true : !isInstalled}
+          >
+            <option value={'best'}>Best</option>
+            <option value={'medium'}>Medium</option>
+            <option value={'low'}>Low</option>
+          </select>
+        </div>
       </div>
 
       {/*
         Options avancées
         */}
-      <h2>Options avancées</h2>
-      <div>
-        <label>Playlist</label>
-        <input
-          checked={playlist}
-          onChange={(e) => setPlaylist(e.target.checked)}
-          disabled={isInstalled === null ? true : !isInstalled}
-          type={'checkbox'}
-        />
+      <div className={'yt-dlp-advanced-input'}>
+        <h4 className={'title'} onClick={() => setShowAdvanced((e) => !e)}>
+          Options avancées
+          <img
+            src={showAdvanced ? right_arrow_icon : bottom_arrow_icon}
+            alt={'advanced input icon'}
+            className={'icon'}
+          />
+        </h4>
+        {showAdvanced ? (
+          <>
+            <div className={'yt-dlp-advanced-module'}>
+              <label className={'label'}>Playlist</label>
+              <input
+                checked={playlist}
+                onChange={(e) => setPlaylist(e.target.checked)}
+                disabled={isInstalled === null ? true : !isInstalled}
+                type={'checkbox'}
+              />
+            </div>
+            <div className={'yt-dlp-advanced-module incoming_feature'}>
+              <span className={'label'}>Type de fichier</span>
+              <select>
+                <option value={''}>Auto</option>
+                {audioEXT.map((ext, i) => {
+                  return (
+                    <option value={ext} key={i}>
+                      {ext}
+                    </option>
+                  )
+                })}
+              </select>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
-      <div>
-        <span>Type de fichier</span>
-        <select>
-          <option value={''}>Auto</option>
-          {audioEXT.map((ext, i) => {
-            return (
-              <option value={ext} key={i}>
-                {ext}
-              </option>
-            )
-          })}
-        </select>
-      </div>
-
       {/*
         Bouton Download
         */}
       <div className={'yt-dlp-button'}>
-        <img onClick={downloadRequest} src={extract_icon} className={'icon'} alt={'extract icon'} />
+        <img
+          onClick={downloadRequest}
+          src={extract_icon}
+          className={`icon ${disabled ? 'disabled' : ''}`}
+          alt={'extract icon'}
+        />
       </div>
     </div>
   )
