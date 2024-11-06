85a543b6a6daa4c5bb6a3efe3dd6e23d0ce1dd02
mayo <maxinou68.lr@gmail.com>
2024-11-06 22:01:32 +0100

    Refactor: ipcHandler.ts Réduction du album pathname

--- src/main/ipcHandler.ts
+++ src/main/ipcHandler.ts
@@ -40,26 +40,22 @@ function ipcLibrary(): void {
       let cover: undefined | string = undefined
       let order: undefined | string[] = []
 
+      const album_pathname = `${AppSettings().settings.savePath}/MayoMusic/${folder}`
+
       // Si fichier de configuration
-      if (fs.existsSync(`${AppSettings().settings.savePath}/MayoMusic/${folder}/setting.json`)) {
+      if (fs.existsSync(`${album_pathname}/setting.json`)) {
         const music_setting: music_setting = JSON.parse(
-          fs.readFileSync(
-            `${AppSettings().settings.savePath}/MayoMusic/${folder}/setting.json`,
-            'utf-8'
-          )
+          fs.readFileSync(`${album_pathname}/setting.json`, 'utf-8')
         )
         if (music_setting.cover) {
-          cover = fs.readFileSync(
-            `${AppSettings().settings.savePath}/MayoMusic/${folder}/${music_setting.cover}`,
-            'base64'
-          )
+          cover = fs.readFileSync(`${album_pathname}/${music_setting.cover}`, 'base64')
         }
         if (music_setting.order) {
           order = music_setting.order
         }
       } else {
         order = fs
-          .readdirSync(`${AppSettings().settings.savePath}/MayoMusic/${folder}/`)
+          .readdirSync(`${album_pathname}/`)
           .filter((e) =>
             ['.ogg', '.mp3', '.webm', '.m4a', '.opus'].includes(path.extname(e).toLowerCase())
           )
@@ -68,7 +64,7 @@ function ipcLibrary(): void {
       // On push toute les info dans la library
       library.push({
         title: folder,
-        path: `${AppSettings().settings.savePath}/MayoMusic/${folder}`,
+        path: `${album_pathname}`,
         cover: cover ? `data:image/png;base64,${cover}` : undefined,
         order: order
       })
@@ -101,9 +97,10 @@ function ipcLibrary(): void {
 
   // EVENT PLAYER
   // File d'attente de musique
-  const queue: { albumName: string; order: string[] } = {
+  const queue: { albumName: string; order: string[]; path: string } = {
     albumName: '',
-    order: []
+    order: [],
+    path: ''
   }
   // Start a music
   ipcMain.on('sendMusic', (event, args: { album: string; index: number }) => {
@@ -111,14 +108,12 @@ function ipcLibrary(): void {
     const musicName = album.order[args.index]
 
     // Audio file
-    const audio = fs.readFileSync(
-      `${AppSettings().settings.savePath}/MayoMusic/${args.album}/${musicName}`,
-      'base64'
-    )
+    const audio = fs.readFileSync(`${album.path}/${musicName}`, 'base64')
 
     // Audio queue
     queue.order = album.order
     queue.albumName = album.title
+    queue.path = album.path
 
     // Response
     event.sender.send('playMusic', {
@@ -138,10 +133,8 @@ function ipcLibrary(): void {
       nextMusic = 0
     }
 
-    const audio = fs.readFileSync(
-      `${AppSettings().settings.savePath}/MayoMusic/${queue.albumName}/${queue.order[nextMusic]}`,
-      'base64'
-    )
+    // Get audio file
+    const audio = fs.readFileSync(`${queue.path}/${queue.order[nextMusic]}`, 'base64')
 
     event.sender.send('playMusic', {
       name: queue.order[nextMusic],
@@ -155,15 +148,12 @@ function ipcLibrary(): void {
     if (queue.albumName === '' || args === null) return
 
     let nextMusic = args - 1
-    console.log(nextMusic)
     if (nextMusic < 0) {
       nextMusic = queue.order.length - 1
     }
-    console.log(nextMusic)
-    const audio = fs.readFileSync(
-      `${AppSettings().settings.savePath}/MayoMusic/${queue.albumName}/${queue.order[nextMusic]}`,
-      'base64'
-    )
+
+    // Get audio file
+    const audio = fs.readFileSync(`${queue.path}/${queue.order[nextMusic]}`, 'base64')
 
     event.sender.send('playMusic', {
       name: queue.order[nextMusic],
