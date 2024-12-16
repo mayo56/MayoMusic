723ae3f159ae13beb6e14253a75ba76ecc1f5150
mayo <maxinou68.lr@gmail.com>
2024-12-16 08:12:01 +0100

    Mise à jour des routes
    Passage au HashRouter pour Windows
    (Version non testé)

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
+// Import React
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
+import React from 'react'
+
+// Layout
+import Settings from '@renderer/Pages/Settings'
 import Global from './Pages/Global'
-import { BrowserRouter } from 'react-router-dom'
+
+// Import pages
+import YoutubeDLPage from '@renderer/Pages/Sous-Pages/Settings/Download/Youtube-DL'
+import ComingSoon from '@renderer/Pages/Sous-Pages/Settings/Error/CommingSoon'
+import MusicList from '@renderer/Pages/Sous-Pages/Library/MusicList'
+import Album from '@renderer/Pages/Sous-Pages/Library/Album'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Global />
-    </BrowserRouter>
+    <HashRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
+      <Routes>
+        {/* Layout Global */}
+        <Route element={<Global />}>
+          {/* Page d'info */}
+          <Route index element={<p>[INFO] - Page d&#39;accueille</p>} />
+
+          {/* Routes Library */}
+          <Route path={'/library'}>
+            <Route index element={<MusicList />} />
+            <Route path={':id'} element={<Album />} />
+          </Route>
+
+          {/* Routes Paramètres */}
+          <Route path={'/settings'} element={<Settings />}>
+            <Route path={'download/yt-dlp'} element={<YoutubeDLPage />} />
+            <Route path={'*'} element={<ComingSoon />} />
+          </Route>
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 )
